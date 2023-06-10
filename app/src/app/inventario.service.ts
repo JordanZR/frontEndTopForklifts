908cc6a196aadb1237764a8a04c3a245d80c0868
JordanZ <jordan.zelayahs@gmail.com>
2023-06-09 19:06:18 -0600

    Changed fechaModificacion

--- app/src/app/inventario.service.ts
+++ app/src/app/inventario.service.ts
@@ -13,23 +13,39 @@ export class InventarioService {
     return this.http.get<any>(this.apiUrl)
   }
   postInventario(nombre_:string, modelo_:string, cantidad_:number, precio_:number):Observable<any>{
+    const currentDate = new Date();
+    const formattedDate = currentDate.toLocaleString("en-US", {
+      month: "numeric",
+      day: "numeric",
+      year: "numeric",
+      hour: "numeric",
+      minute: "numeric"
+    });
     return this.http.post(this.apiUrl,{
       nombre:nombre_,
       modelo:modelo_,
       cantidad:cantidad_,
       precio:precio_,
       trabajador_modificacion_id:1,
-      fecha_modificacion:"06/03/2023"
+      fecha_modificacion:formattedDate
     })
   }
   putInventario(nombre_:string, modelo_:string, cantidad_:number, precio_:number, id_:number):Observable<any>{
+    const currentDate = new Date();
+    const formattedDate = currentDate.toLocaleString("en-US", {
+      month: "numeric",
+      day: "numeric",
+      year: "numeric",
+      hour: "numeric",
+      minute: "numeric"
+    });
     return this.http.put(this.apiUrl,{
       nombre:nombre_,
       modelo:modelo_,
       cantidad:cantidad_,
       precio:precio_,
       trabajador_modificacion_id:1,
-      fecha_modificacion:"06/04/2023",
+      fecha_modificacion:formattedDate,
       id:id_
     })
   }
